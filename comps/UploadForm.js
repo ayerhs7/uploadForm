130b1cc8f3be974a456fd8a8e1e16678373f0f26
ayerhs7 <62745351+ayerhs7@users.noreply.github.com>
2020-08-20 10:12:07 +0530

    Update UploadForm.js

--- comps/UploadForm.js
+++ comps/UploadForm.js
@@ -5,7 +5,7 @@ const UploadForm = () => {
   const [file, setFile] = useState(null);
   const [error, setError] = useState(null);
 
-  const types = ['image/png', 'image/jpeg'];
+  const types = ['video/mp4', 'video/mkv', 'pdf', 'image/jpeg'];
 
   const handleChange = (e) => {
     let selected = e.target.files[0];
@@ -15,7 +15,7 @@ const UploadForm = () => {
       setError('');
     } else {
       setFile(null);
-      setError('Please select an image file (png or jpg)');
+      setError('Please select an valid file ');
     }
   };
 
